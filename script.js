d1cabe56ec59a34e11606196307c90ee4ec93348
Elenar9 <dannyelenar@gmail.com>
2023-05-23 12:12:39 +0100

    tv-show-dom-project
    create level 100

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 //You can edit ALL of the code here
+let allEpisodes = getAllEpisodes();
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.textContent = `Got ${episodeList.length} episode(s) All this data is from TVMaze.com`;
 }
+
+//  level 100
+let allEp = document.getElementById("allEp");
+
+episodeList.forEach((el) => {
+  let episodeDiv = document.createElement("div"); 
+  episodeDiv.classList.add("episodeCard");
+
+  episodeDiv.innerHTML =`
+  <h2>${el.name} - S${el.season.toString().padStart(2,"0")}E${el.number.toString().padStart(2,"0")}</h2>
+  <img src="${el.image.medium}">
+    <span>${el.summary}</span>
+    `;
+
+    allEp.appendChild(episodeDiv);
+
+});
 
 window.onload = setup;
